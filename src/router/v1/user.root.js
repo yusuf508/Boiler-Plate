1bd47450d35eb034dc5101014fbc11f6c84f58af
yusuf508 <yuusufcarab84@gmail.com>
2021-11-19 11:17:21 -0800

    updated queries

--- src/router/v1/user.root.js
+++ src/router/v1/user.root.js
@@ -10,8 +10,10 @@ const accessToken = require('../../midleware/auth');
 router.get('/getAllUsers', usercontroler.getALLusers);
 router.post('/userById',usercontroler.getsingleUser);
 router.post('/create',validate(uservalidator.createUser),usercontroler.create);
-router.post('/updateUser',validate(uservalidator.updateUser),usercontroler.updateUsers);
+//router.post('/updateUser',validate(uservalidator.updateUser),usercontroler.updateUsers);
 router.post('/delete',usercontroler.deleteUser);
+
+router.post('/updateUser',accessToken.auth,validate(uservalidator.updateUser), accessToken.authentication('updateUser'),usercontroler.updateUsers)
  
  
 
